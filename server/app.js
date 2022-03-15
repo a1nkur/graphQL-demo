617d855cd8b3bcb17ef924e69a4977a923740693
ankur29mac <ankur29mac@gmail.com>
2022-03-15 11:05:58 +0530

    added schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
 
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass in a schema property
+  })
+);
+
 app.listen(4000, () => {
-  console.log("Now listening for requests on port 4000.");
+  console.log("now listening for requests on port 4000");
 });
